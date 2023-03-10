92458d4583d999809eb7f5a53b82bd964033658d
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-03-10 18:54:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,14 +8,8 @@
 # }
 
 terraform {
-  required_version = ">= 0.14"
-  required_providers {
-    aws = {
-          source  = "hashicorp/aws"
-          version = ">= 3.35.0"
-        }
-      }
-    }
+  required_version = ">= 0.13"
+}
 # Configure and downloading plugins for aws
 provider "aws" {
   region     = "${var.aws_region}"
