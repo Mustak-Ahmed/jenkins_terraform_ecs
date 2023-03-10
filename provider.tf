755b689d39d624e3764dcd15f9f43b80d8f70c02
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-03-10 18:40:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,16 @@
 #   }
 # }
 
+terraform {
+  required_providers {
+...
+    rke = {
+      source  = "rancher/rke"
+      version = "1.1.0"
+    }
+  }
+...
+}
 # Configure and downloading plugins for aws
 provider "aws" {
   region     = "${var.aws_region}"
