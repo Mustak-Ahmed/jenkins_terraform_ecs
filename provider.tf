400010c4d4f3c2d1f3e78217bb84029e33ca2883
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-03-10 18:08:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
 
 # Configure and downloading plugins for aws
 provider "aws" {
